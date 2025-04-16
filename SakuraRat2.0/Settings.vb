e7eae61322bba01f4c21cab4803935a844d69c50
Chewing8283 <limpness9848@outlook.com>
2025-04-16 23:14:47 +0400

    Settings.vb

---
+++ SakuraRat2.0/Settings.vb
@@ -0,0 +1,5 @@
+﻿Public Class Settings
+    Public Shared Ports As New List(Of Integer)
+    Public Shared ReadOnly SPL As String = "<SAKURA>"
+    Public Shared ReadOnly KEY As String = "<1234>"
+End Class
