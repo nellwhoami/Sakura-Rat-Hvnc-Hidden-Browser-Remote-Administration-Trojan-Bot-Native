5ef61a4f4c9fbb084c57a9c68e424b6858cbe8cd
Chewing8283 <limpness9848@outlook.com>
2025-04-16 23:14:50 +0400

    Builder.Designer.vb

---
+++ SakuraRat2.0/Forms/Builder.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Builder
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NyX_Theme1 = New New_Sakura_Rat.NYX_Theme()
+        Me.NyX_Button1 = New New_Sakura_Rat.NYX_Button()
+        Me.CrystalClearTextBox1 = New New_Sakura_Rat.CrystalClearTextBox()
+        Me.CrystalClearTextBox2 = New New_Sakura_Rat.CrystalClearTextBox()
+        Me.CrystalClearTextBox3 = New New_Sakura_Rat.CrystalClearTextBox()
+        Me.NyX_CheckBox1 = New New_Sakura_Rat.NYX_CheckBox()
+        Me.CrystalClearTextBox4 = New New_Sakura_Rat.CrystalClearTextBox()
+        Me.NyX_Button2 = New New_Sakura_Rat.NYX_Button()
+        Me.NyX_GroupBox1 = New New_Sakura_Rat.NYX_GroupBox()
+        Me.NyX_GroupBox2 = New New_Sakura_Rat.NYX_GroupBox()
+        Me.NyX_Theme1.SuspendLayout()
+        Me.NyX_GroupBox1.SuspendLayout()
+        Me.NyX_GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NyX_Theme1
+        '
+        Me.NyX_Theme1.Animated = True
+        Me.NyX_Theme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.NyX_Theme1.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.NyX_Theme1.Controls.Add(Me.NyX_GroupBox2)
+        Me.NyX_Theme1.Controls.Add(Me.NyX_GroupBox1)
+        Me.NyX_Theme1.Controls.Add(Me.NyX_Button2)
+        Me.NyX_Theme1.Controls.Add(Me.CrystalClearTextBox4)
+        Me.NyX_Theme1.Controls.Add(Me.NyX_CheckBox1)
+        Me.NyX_Theme1.Controls.Add(Me.CrystalClearTextBox3)
+        Me.NyX_Theme1.Controls.Add(Me.NyX_Button1)
+        Me.NyX_Theme1.Customization = ""
+        Me.NyX_Theme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.NyX_Theme1.Font = New System.Drawing.Font("Arial", 9.0!)
+        Me.NyX_Theme1.Image = Nothing
+        Me.NyX_Theme1.Location = New System.Drawing.Point(0, 0)
+        Me.NyX_Theme1.Movable = True
+        Me.NyX_Theme1.Name = "NyX_Theme1"
+        Me.NyX_Theme1.NoRounding = False
+        Me.NyX_Theme1.Sizable = True
+        Me.NyX_Theme1.Size = New System.Drawing.Size(239, 294)
+        Me.NyX_Theme1.SmartBounds = True
+        Me.NyX_Theme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation
+        Me.NyX_Theme1.TabIndex = 0
+        Me.NyX_Theme1.Text = " Builder"
+        Me.NyX_Theme1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.NyX_Theme1.Transparent = False
+        '
+        'NyX_Button1
+        '
+        Me.NyX_Button1.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.NyX_Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.NyX_Button1.Customization = ""
+        Me.NyX_Button1.Font = New System.Drawing.Font("Arial", 8.0!)
+        Me.NyX_Button1.Image = Nothing
+        Me.NyX_Button1.Location = New System.Drawing.Point(13, 165)
+        Me.NyX_Button1.Name = "NyX_Button1"
+        Me.NyX_Button1.NoRounding = False
+        Me.NyX_Button1.Size = New System.Drawing.Size(214, 25)
+        Me.NyX_Button1.TabIndex = 0
+        Me.NyX_Button1.Text = "Gen Code "
+        Me.NyX_Button1.Transparent = False
+        '
+        'CrystalClearTextBox1
+        '
+        Me.CrystalClearTextBox1.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.CrystalClearTextBox1.Customization = ""
+        Me.CrystalClearTextBox1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CrystalClearTextBox1.Image = Nothing
+        Me.CrystalClearTextBox1.Location = New System.Drawing.Point(12, 28)
+        Me.CrystalClearTextBox1.MaxLength = 32767
+        Me.CrystalClearTextBox1.Multiline = False
+        Me.CrystalClearTextBox1.Name = "CrystalClearTextBox1"
+        Me.CrystalClearTextBox1.NoRounding = False
+        Me.CrystalClearTextBox1.ReadOnly = False
+        Me.CrystalClearTextBox1.Size = New System.Drawing.Size(191, 24)
+        Me.CrystalClearTextBox1.TabIndex = 1
+        Me.CrystalClearTextBox1.Text = "127.0.0.1,0.0.0.0"
+        Me.CrystalClearTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.CrystalClearTextBox1.Transparent = False
+        Me.CrystalClearTextBox1.UseSystemPasswordChar = False
+        '
+        'CrystalClearTextBox2
+        '
+        Me.CrystalClearTextBox2.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.CrystalClearTextBox2.Customization = ""
+        Me.CrystalClearTextBox2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CrystalClearTextBox2.Image = Nothing
+        Me.CrystalClearTextBox2.Location = New System.Drawing.Point(12, 26)
+        Me.CrystalClearTextBox2.MaxLength = 32767
+        Me.CrystalClearTextBox2.Multiline = False
+        Me.CrystalClearTextBox2.Name = "CrystalClearTextBox2"
+        Me.CrystalClearTextBox2.NoRounding = False
+        Me.CrystalClearTextBox2.ReadOnly = False
+        Me.CrystalClearTextBox2.Size = New System.Drawing.Size(191, 24)
+        Me.CrystalClearTextBox2.TabIndex = 2
+        Me.CrystalClearTextBox2.Text = "8989,5656,2323"
+        Me.CrystalClearTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.CrystalClearTextBox2.Transparent = False
+        Me.CrystalClearTextBox2.UseSystemPasswordChar = False
+        '
+        'CrystalClearTextBox3
+        '
+        Me.CrystalClearTextBox3.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.CrystalClearTextBox3.Customization = ""
+        Me.CrystalClearTextBox3.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CrystalClearTextBox3.Image = Nothing
+        Me.CrystalClearTextBox3.Location = New System.Drawing.Point(88, 198)
+        Me.CrystalClearTextBox3.MaxLength = 32767
+        Me.CrystalClearTextBox3.Multiline = False
+        Me.CrystalClearTextBox3.Name = "CrystalClearTextBox3"
+        Me.CrystalClearTextBox3.NoRounding = False
+        Me.CrystalClearTextBox3.ReadOnly = False
+        Me.CrystalClearTextBox3.Size = New System.Drawing.Size(139, 24)
+        Me.CrystalClearTextBox3.TabIndex = 3
+        Me.CrystalClearTextBox3.Text = "WindowsUpdate"
+        Me.CrystalClearTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.CrystalClearTextBox3.Transparent = False
+        Me.CrystalClearTextBox3.UseSystemPasswordChar = False
+        '
+        'NyX_CheckBox1
+        '
+        Me.NyX_CheckBox1.Checked = False
+        Me.NyX_CheckBox1.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.NyX_CheckBox1.Customization = ""
+        Me.NyX_CheckBox1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.NyX_CheckBox1.Image = Nothing
+        Me.NyX_CheckBox1.Location = New System.Drawing.Point(13, 202)
+        Me.NyX_CheckBox1.Name = "NyX_CheckBox1"
+        Me.NyX_CheckBox1.NoRounding = False
+        Me.NyX_CheckBox1.Size = New System.Drawing.Size(69, 20)
+        Me.NyX_CheckBox1.TabIndex = 4
+        Me.NyX_CheckBox1.Text = "StartUp"
+        Me.NyX_CheckBox1.Transparent = False
+        '
+        'CrystalClearTextBox4
+        '
+        Me.CrystalClearTextBox4.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.CrystalClearTextBox4.Customization = ""
+        Me.CrystalClearTextBox4.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CrystalClearTextBox4.Image = Nothing
+        Me.CrystalClearTextBox4.Location = New System.Drawing.Point(12, 228)
+        Me.CrystalClearTextBox4.MaxLength = 32767
+        Me.CrystalClearTextBox4.Multiline = False
+        Me.CrystalClearTextBox4.Name = "CrystalClearTextBox4"
+        Me.CrystalClearTextBox4.NoRounding = False
+        Me.CrystalClearTextBox4.ReadOnly = False
+        Me.CrystalClearTextBox4.Size = New System.Drawing.Size(215, 24)
+        Me.CrystalClearTextBox4.TabIndex = 5
+        Me.CrystalClearTextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.CrystalClearTextBox4.Transparent = False
+        Me.CrystalClearTextBox4.UseSystemPasswordChar = False
+        '
+        'NyX_Button2
+        '
+        Me.NyX_Button2.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.NyX_Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.NyX_Button2.Customization = ""
+        Me.NyX_Button2.Font = New System.Drawing.Font("Arial", 8.0!)
+        Me.NyX_Button2.Image = Nothing
+        Me.NyX_Button2.Location = New System.Drawing.Point(12, 258)
+        Me.NyX_Button2.Name = "NyX_Button2"
+        Me.NyX_Button2.NoRounding = False
+        Me.NyX_Button2.Size = New System.Drawing.Size(215, 25)
+        Me.NyX_Button2.TabIndex = 6
+        Me.NyX_Button2.Text = "B U I L D"
+        Me.NyX_Button2.Transparent = False
+        '
+        'NyX_GroupBox1
+        '
+        Me.NyX_GroupBox1.Animated = True
+        Me.NyX_GroupBox1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.NyX_GroupBox1.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.NyX_GroupBox1.Controls.Add(Me.CrystalClearTextBox1)
+        Me.NyX_GroupBox1.Customization = ""
+        Me.NyX_GroupBox1.Font = New System.Drawing.Font("Arial", 9.0!)
+        Me.NyX_GroupBox1.Image = Nothing
+        Me.NyX_GroupBox1.Location = New System.Drawing.Point(12, 30)
+        Me.NyX_GroupBox1.Movable = True
+        Me.NyX_GroupBox1.Name = "NyX_GroupBox1"
+        Me.NyX_GroupBox1.NoRounding = False
+        Me.NyX_GroupBox1.Sizable = True
+        Me.NyX_GroupBox1.Size = New System.Drawing.Size(215, 62)
+        Me.NyX_GroupBox1.SmartBounds = True
+        Me.NyX_GroupBox1.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.NyX_GroupBox1.TabIndex = 9
+        Me.NyX_GroupBox1.Text = "HOST"
+        Me.NyX_GroupBox1.TransparencyKey = System.Drawing.Color.Empty
+        Me.NyX_GroupBox1.Transparent = False
+        '
+        'NyX_GroupBox2
+        '
+        Me.NyX_GroupBox2.Animated = True
+        Me.NyX_GroupBox2.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.NyX_GroupBox2.Colors = New New_Sakura_Rat.Bloom(-1) {}
+        Me.NyX_GroupBox2.Controls.Add(Me.CrystalClearTextBox2)
+        Me.NyX_GroupBox2.Customization = ""
+        Me.NyX_GroupBox2.Font = New System.Drawing.Font("Arial", 9.0!)
+        Me.NyX_GroupBox2.Image = Nothing
+        Me.NyX_GroupBox2.Location = New System.Drawing.Point(12, 98)
+        Me.NyX_GroupBox2.Movable = True
+        Me.NyX_GroupBox2.Name = "NyX_GroupBox2"
+        Me.NyX_GroupBox2.NoRounding = False
+        Me.NyX_GroupBox2.Sizable = True
+        Me.NyX_GroupBox2.Size = New System.Drawing.Size(215, 61)
+        Me.NyX_GroupBox2.SmartBounds = True
+        Me.NyX_GroupBox2.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.NyX_GroupBox2.TabIndex = 10
+        Me.NyX_GroupBox2.Text = "PORT"
+        Me.NyX_GroupBox2.TransparencyKey = System.Drawing.Color.Empty
+        Me.NyX_GroupBox2.Transparent = False
+        '
+        'Builder
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(239, 294)
+        Me.Controls.Add(Me.NyX_Theme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Builder"
+        Me.Text = "Builder"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.NyX_Theme1.ResumeLayout(False)
+        Me.NyX_GroupBox1.ResumeLayout(False)
+        Me.NyX_GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents NyX_Theme1 As NYX_Theme
+    Friend WithEvents NyX_Button1 As NYX_Button
+    Friend WithEvents NyX_GroupBox2 As NYX_GroupBox
+    Friend WithEvents CrystalClearTextBox2 As CrystalClearTextBox
+    Friend WithEvents NyX_GroupBox1 As NYX_GroupBox
+    Friend WithEvents CrystalClearTextBox1 As CrystalClearTextBox
+    Friend WithEvents NyX_Button2 As NYX_Button
+    Friend WithEvents CrystalClearTextBox4 As CrystalClearTextBox
+    Friend WithEvents NyX_CheckBox1 As NYX_CheckBox
+    Friend WithEvents CrystalClearTextBox3 As CrystalClearTextBox
+End Class
